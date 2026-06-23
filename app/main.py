849d122bf4ee0bad45778694b1341c60a44398cc
uniquetshirtdesigns <140870441+smartsamy41@users.noreply.github.com>
2026-06-24 00:57:56 +0200

    Update main.py

--- app/main.py
+++ app/main.py
@@ -5,3 +5,11 @@ app = FastAPI()
 @app.get("/")
 def root():
     return {"status": "OK"}
+
+@app.get("/run")
+def run():
+    return {"status": "RUN OK"}
+
+@app.get("/health")
+def health():
+    return {"status": "HEALTH OK"}
