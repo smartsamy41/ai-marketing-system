15a75679571411bc29dd099373b1449d32412426
uniquetshirtdesigns <140870441+smartsamy41@users.noreply.github.com>
2026-07-06 18:37:27 +0200

    Create youtube_real.py

---
+++ engine/youtube_real.py
@@ -0,0 +1,32 @@
+from googleapiclient.discovery import build
+from googleapiclient.http import MediaFileUpload
+import os
+
+class YouTubeReal:
+
+    def __init__(self, credentials):
+
+        self.youtube = build("youtube", "v3", credentials=credentials)
+
+    # =========================
+    # UPLOAD VIDEO
+    # =========================
+    def upload_video(self, file_path, title, description):
+
+        request = self.youtube.videos().insert(
+            part="snippet,status",
+            body={
+                "snippet": {
+                    "title": title,
+                    "description": description
+                },
+                "status": {
+                    "privacyStatus": "public"
+                }
+            },
+            media_body=MediaFileUpload(file_path)
+        )
+
+        response = request.execute()
+
+        return response
