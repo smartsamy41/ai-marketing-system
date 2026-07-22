de5bc61e7427544b05724b602fd5c9ac55c60048
Samy Jendoubi <samyjendoubi@gmail.com>
2026-07-22 18:01:13 +0000

    Add TikTok Pixel base code to website head

--- app/main.py
+++ app/main.py
@@ -454,6 +454,44 @@ def render_page(
           content="width=device-width, initial-scale=1">
 
 
+
+
+
+
+    <!-- TikTok Pixel Code Start -->
+    <script>
+    !function (w, d, t) {{
+      w.TiktokAnalyticsObject=t;
+      var ttq=w[t]=w[t]||[];
+      ttq.methods=["page","track","identify","instances","debug","on","off","once","ready","alias","group","enableCookie","disableCookie","holdConsent","revokeConsent","grantConsent"];
+      ttq.setAndDefer=function(t,e){{
+        t[e]=function(){{
+          t.push([e].concat(Array.prototype.slice.call(arguments,0)))
+        }}
+      }};
+      for(var i=0;i<ttq.methods.length;i++)
+        ttq.setAndDefer(ttq,ttq.methods[i]);
+
+      ttq.load=function(e,n){{
+        var r="https://analytics.tiktok.com/i18n/pixel/events.js";
+        ttq._i=ttq._i||{{}};
+        ttq._i[e]=[];
+        ttq._i[e]._u=r;
+        n=document.createElement("script");
+        n.type="text/javascript";
+        n.async=!0;
+        n.src=r+"?sdkid="+e+"&lib="+t;
+        e=document.getElementsByTagName("script")[0];
+        e.parentNode.insertBefore(n,e);
+      }};
+
+      ttq.load('D9GFF7JC77U67C7IKFDG');
+      ttq.page();
+
+    }}(window, document, 'ttq');
+    </script>
+    <!-- TikTok Pixel Code End -->
+
     <!-- Google Analytics 4 -->
     <script async src="https://www.googletagmanager.com/gtag/js?id=G-EG6E1DPH35"></script>
     <script>
